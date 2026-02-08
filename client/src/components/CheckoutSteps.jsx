89b5d8cfcb3870f8154811d92a1ef4673a3640bc
Dilakshan <dsivarub@my.centennialcollege.ca>
2026-02-08 15:52:51 -0500

    style: Force white text color in checkout and auth forms

--- client/src/components/CheckoutSteps.jsx
+++ client/src/components/CheckoutSteps.jsx
@@ -5,16 +5,16 @@ const CheckoutSteps = ({ step1, step2, step3, step4 }) => {
     return (
         <div style={{ display: 'flex', justifyContent: 'center', marginBottom: '20px' }}>
             <div style={{ margin: '0 20px' }}>
-                {step1 ? <Link to='/login'>Sign In</Link> : <span style={{ color: '#ccc' }}>Sign In</span>}
+                {step1 ? <Link to='/login' style={{ color: '#fff' }}>Sign In</Link> : <span style={{ color: 'rgba(255, 255, 255, 0.5)' }}>Sign In</span>}
             </div>
             <div style={{ margin: '0 20px' }}>
-                {step2 ? <Link to='/shipping'>Shipping</Link> : <span style={{ color: '#ccc' }}>Shipping</span>}
+                {step2 ? <Link to='/shipping' style={{ color: '#fff' }}>Shipping</Link> : <span style={{ color: 'rgba(255, 255, 255, 0.5)' }}>Shipping</span>}
             </div>
             <div style={{ margin: '0 20px' }}>
-                {step3 ? <Link to='/payment'>Payment</Link> : <span style={{ color: '#ccc' }}>Payment</span>}
+                {step3 ? <Link to='/payment' style={{ color: '#fff' }}>Payment</Link> : <span style={{ color: 'rgba(255, 255, 255, 0.5)' }}>Payment</span>}
             </div>
             <div style={{ margin: '0 20px' }}>
-                {step4 ? <Link to='/placeorder'>Place Order</Link> : <span style={{ color: '#ccc' }}>Place Order</span>}
+                {step4 ? <Link to='/placeorder' style={{ color: '#fff' }}>Place Order</Link> : <span style={{ color: 'rgba(255, 255, 255, 0.5)' }}>Place Order</span>}
             </div>
         </div>
     );
